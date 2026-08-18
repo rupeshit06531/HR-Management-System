72b6b9db1c954d40caa5d744aa15de7f2c33c848
Rupesh Kumar Mahto <rupeshit06531@gmail.com>
2026-08-18 12:42:48 +0530

    Load enterprise HRMS UI styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 
 import "./index.css"
+import "./App.css"
 import App from "./App.tsx"
 import { AuthProvider } from "./context/AuthContext"
 
