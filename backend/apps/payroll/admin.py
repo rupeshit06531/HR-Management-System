830ecb6332f39f680afb721ed06da4c61ceabace
Rupesh Kumar Mahto <rupeshit06531@gmail.com>
2026-08-18 16:29:47 +0530

    Improve payroll admin configuration

--- backend/apps/payroll/admin.py
+++ backend/apps/payroll/admin.py
@@ -1,3 +1,4 @@
+
 from django.contrib import admin
 
 from .models import Payroll
@@ -14,17 +15,42 @@ class PayrollAdmin(admin.ModelAdmin):
         "deductions",
         "net_salary",
         "payment_status",
+        "paid_at",
+        "created_at",
+        "updated_at",
     )
 
     search_fields = (
         "employee__employee_id",
+        "employee__user__username",
         "employee__user__first_name",
         "employee__user__last_name",
+        "employee__user__email",
     )
 
     list_filter = (
         "payment_status",
         "month",
+        "paid_at",
     )
 
-    ordering = ("-month",)
+    ordering = (
+        "-month",
+        "-created_at",
+        "-id",
+    )
+
+    list_select_related = (
+        "employee",
+        "employee__user",
+    )
+
+    readonly_fields = (
+        "net_salary",
+        "created_at",
+        "updated_at",
+    )
+
+    date_hierarchy = "month"
+
+    list_per_page = 50
